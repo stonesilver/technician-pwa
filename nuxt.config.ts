34dc5579f009c14fb0a8e0c9e3b78f090cd9495e
stonesilver <stonesilver12@gmail.com>
2025-03-29 13:10:59 +0100

    chore: checking caching issue fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,8 +10,7 @@ export default defineNuxtConfig({
     //   cert: "./localhost+2.pem",
     // },
   },
-  // "@vite-pwa/nuxt",
-  modules: ["@nuxt/image", "@nuxtjs/tailwindcss", "shadcn-nuxt", "@vee-validate/nuxt"],
+  modules: ["@vite-pwa/nuxt", "@nuxt/image", "@nuxtjs/tailwindcss", "shadcn-nuxt", "@vee-validate/nuxt"],
   veeValidate: {
     // disable or enable auto imports
     autoImports: true,
@@ -32,108 +31,108 @@ export default defineNuxtConfig({
      */
     componentDir: "./components/ui",
   },
-  // pwa: {
-  //   registerType: "autoUpdate", // "prompt",
-  //   strategies: "injectManifest",
-  //   srcDir: "./service-worker",
-  //   filename: "sw.ts",
-  //   manifest: {
-  //     id: "/?mca=0.0.0",
-  //     name: "Auto Quote Estimator",
-  //     short_name: "MyCover.ai",
-  //     description:
-  //       "A comprehensive platform designed for auto technicians and parts dealers to quickly and accurately generate pricing estimates for damaged parts and associated workmanship",
-  //     theme_color: "#3BAA90",
-  //     icons: [
-  //       {
-  //         src: "app-icons/app-icons-48x48.png",
-  //         sizes: "48x48",
-  //         type: "image/png",
-  //       },
-  //       {
-  //         src: "app-icons/app-icons-72x72.png",
-  //         sizes: "72x72",
-  //         type: "image/png",
-  //       },
-  //       {
-  //         src: "app-icons/app-icons-96x96.png",
-  //         sizes: "96x96",
-  //         type: "image/png",
-  //       },
-  //       {
-  //         src: "app-icons/app-icons-128x128.png",
-  //         sizes: "128x128",
-  //         type: "image/png",
-  //       },
-  //       {
-  //         src: "app-icons/app-icons-192x192.png",
-  //         sizes: "192x192",
-  //         type: "image/png",
-  //         purpose: "any maskable",
-  //       },
-  //       {
-  //         src: "app-icons/app-icons-384x384.png",
-  //         sizes: "384x384",
-  //         type: "image/png",
-  //       },
-  //       {
-  //         src: "app-icons/app-icons-512x512.png",
-  //         sizes: "512x512",
-  //         type: "image/png",
-  //       },
-  //     ],
-  //     screenshots: [
-  //       {
-  //         src: "app-icons/screenshot-login.png",
-  //         sizes: "1500x3376",
-  //         type: "image/png",
-  //         form_factor: "wide",
-  //       },
-  //       {
-  //         src: "app-icons/screenshot-dashboard.png",
-  //         sizes: "1500x3376",
-  //         type: "image/png",
-  //         form_factor: "narrow",
-  //       },
-  //       {
-  //         src: "app-icons/screenshot-wallet.png",
-  //         sizes: "1500x3376",
-  //         type: "image/png",
-  //         form_factor: "narrow",
-  //       },
-  //     ],
-  //     scope: "/",
-  //     // scope: process.env.VITE_BASE_DOMAIN || "/",
-  //     start_url: "/",
-  //     display: "standalone",
-  //     background_color: "#ffffff",
-  //     prefer_related_applications: true,
-  //     lang: "en",
-  //     orientation: "portrait",
-  //   },
-  //   workbox: {
-  //     cleanupOutdatedCaches: true,
-  //     navigateFallback: "/",
-  //     globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
-  //     navigateFallbackAllowlist: [/^\/$/, /^\/app\/provide-estimate\?.*$/, /^\/app\/$/],
-  //   },
-  //   injectManifest: {
-  //     globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
-  //   },
-  //   client: {
-  //     installPrompt: true,
-  //     // you don't need to include this: only for testing purposes
-  //     // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
-  //     periodicSyncForUpdates: 5000,
-  //   },
-  //   devOptions: {
-  //     enabled: true,
-  //     navigateFallback: "/",
-  //     navigateFallbackAllowlist: [/^\/$/, /^\/app\/provide-estimate\?.*$/],
-  //     type: "module",
-  //     suppressWarnings: true,
-  //   },
-  // },
+  pwa: {
+    registerType: "autoUpdate", // "prompt",
+    strategies: "injectManifest",
+    srcDir: "./service-worker",
+    filename: "service-worker.ts", // "sw.ts",
+    manifest: {
+      id: "/?mca=0.0.0",
+      name: "Auto Quote Estimator",
+      short_name: "MyCover.ai",
+      description:
+        "A comprehensive platform designed for auto technicians and parts dealers to quickly and accurately generate pricing estimates for damaged parts and associated workmanship",
+      theme_color: "#3BAA90",
+      icons: [
+        {
+          src: "app-icons/app-icons-48x48.png",
+          sizes: "48x48",
+          type: "image/png",
+        },
+        {
+          src: "app-icons/app-icons-72x72.png",
+          sizes: "72x72",
+          type: "image/png",
+        },
+        {
+          src: "app-icons/app-icons-96x96.png",
+          sizes: "96x96",
+          type: "image/png",
+        },
+        {
+          src: "app-icons/app-icons-128x128.png",
+          sizes: "128x128",
+          type: "image/png",
+        },
+        {
+          src: "app-icons/app-icons-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "maskable",
+        },
+        {
+          src: "app-icons/app-icons-384x384.png",
+          sizes: "384x384",
+          type: "image/png",
+        },
+        {
+          src: "app-icons/app-icons-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+      screenshots: [
+        {
+          src: "app-icons/screenshot-login.png",
+          sizes: "1500x3376",
+          type: "image/png",
+          form_factor: "wide",
+        },
+        {
+          src: "app-icons/screenshot-dashboard.png",
+          sizes: "1500x3376",
+          type: "image/png",
+          form_factor: "narrow",
+        },
+        {
+          src: "app-icons/screenshot-wallet.png",
+          sizes: "1500x3376",
+          type: "image/png",
+          form_factor: "narrow",
+        },
+      ],
+      scope: "/",
+      // scope: process.env.VITE_BASE_DOMAIN || "/",
+      start_url: "/",
+      display: "standalone",
+      background_color: "#ffffff",
+      prefer_related_applications: true,
+      lang: "en",
+      orientation: "portrait",
+    },
+    workbox: {
+      cleanupOutdatedCaches: true,
+      navigateFallback: "/",
+      globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
+      navigateFallbackAllowlist: [/^\/$/, /^\/app\/provide-estimate\?.*$/, /^\/app\/$/],
+    },
+    injectManifest: {
+      globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
+    },
+    client: {
+      installPrompt: true,
+      // you don't need to include this: only for testing purposes
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      periodicSyncForUpdates: 3600,
+    },
+    devOptions: {
+      enabled: true,
+      navigateFallback: "/",
+      navigateFallbackAllowlist: [/^\/$/, /^\/app\/provide-estimate\?.*$/],
+      type: "module",
+      suppressWarnings: true,
+    },
+  },
   experimental: { appManifest: true },
   vite: {
     server: {
